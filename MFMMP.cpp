cfc26342c799e26db63365b03e58bff2efc934ed
balaji-venkatesh <b.venkatesh@mail.utoronto.ca>
2023-06-15 16:49:54 -0400

    update to actual MM

--- MFMMP.cpp
+++ MFMMP.cpp
@@ -1,17 +1,21 @@
 #include "ap_int.h"
-void MFMMP(
-    ap_uint<32> &din0,
-    ap_uint<32> &din1,
-    ap_uint<32> &dout
-) {
-    #pragma HLS interface axis port=din0
-    #pragma HLS interface axis port=din1
-    #pragma HLS interface axis port=dout
-    #pragma HLS interface ap_ctrl_none port=return
-    ap_uint<32> dout_tmp;
-    for (int i = 0; i < 4; i++) {
-        #pragma HLS unroll
-        dout_tmp(i*8+7,i*8) = ap_uint<8>(din0(i*8+7,i*8)) * ap_uint<8>(din1(i*8+7,i*8));
+
+void MFMMP(ap_uint<512> &din, ap_uint<32> &dout) {
+
+#pragma HLS interface axis port = din
+#pragma HLS interface axis port = dout
+#pragma HLS interface ap_ctrl_none port = return // removes start and finish signals - starts when there is data
+
+    // multiplier
+    ap_uint<256> C;
+    for (int i = 0; i < 8; i++) {
+#pragma HLS unroll
+        C(i * 32 + 31, i * 32) = ap_uint<32>(din(i * 32 + 31, i * 32)) * ap_uint<32>(din(256 + i * 32 + 31, 256 + i * 32)); // C = A * B
     }
+
+    // adder tree
+    ap_uint<32> dout_tmp;
+    dout_tmp = ((C(31, 0) + C(63, 32)) + (C(95, 64) + C(127, 96))) + ((C(159, 128) + C(191, 160)) + (C(223, 192) + C(255, 224)));
+
     dout = dout_tmp;
 }
--- MFMMP_tb.cpp
+++ MFMMP_tb.cpp
@@ -1,38 +1,42 @@
-#include <iostream>
-#include <cstdlib>
 #include "ap_int.h"
-void MFMMP(
-    ap_uint<32> &din0,
-    ap_uint<32> &din1,
-    ap_uint<32> &dout
-);
+#include <cstdlib>
+#include <iostream>
+
+void MFMMP(ap_uint<512> &din, ap_uint<32> &dout);
 
 int main() {
-    ap_uint<32> din0;
-    ap_uint<32> din1;
-    ap_uint<32> dout;
 
+    ap_uint<512> din;
+    ap_uint<32> dout;
     ap_uint<32> expected_output;
 
     int pass = 1;
 
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 4; j++) {
-            din0(j*8+7,j*8) = rand()%256;
-            din1(j*8+7,j*8) = rand()%256;
-            expected_output(j*8+7,j*8) = ap_uint<8>(din0(j*8+7,j*8)) * ap_uint<8>(din1(j*8+7,j*8));
+    for (int i = 0; i < 2; i++) {
+
+        for (int j = 0; j < 16; j++) {
+            din(j * 32 + 31, j * 32) = rand() % 1024;
         }
-        MFMMP(din0,din1,dout);
+
+        for (int j = 0; j < 8; j++) {
+            expected_output(31, 0) = ap_uint<32>(din(j * 32 + 31, j * 32)) * ap_uint<32>(din(256 + j * 32 + 31, 256 + j * 32)) + expected_output(31, 0);
+        }
+
+        MFMMP(din, dout);
+
         if (dout != expected_output) {
-            std::cout << "Test failed at iteration " << i << "\n" <<
-            "input 0 : (" << (din0(7,0)) << "," << (din0(15,8)) << "," << (din0(23,16)) << "," << (din0(31,24)) << ")\n" <<
-            "input 1 : (" << (din1(7,0)) << "," << (din1(15,8)) << "," << (din1(23,16)) << "," << (din1(31,24)) << ")\n" <<
-            "output : (" << (dout(7,0)) << "," << (dout(15,8)) << "," << (dout(23,16)) << "," << (dout(31,24)) << ")\n" <<
-            "expected : (" << (expected_output(7,0)) << "," << (expected_output(15,8)) << "," << (expected_output(23,16)) << "," << (expected_output(31,24)) << ")\n";
+            std::cout << "Test failed at iteration " << i << "\n"
+                      << "A: (" << din(31, 0)    << "," << din(63, 32)   << "," << din(95, 64)   << "," << din(127, 96)  << ","
+                                << din(159, 128) << "," << din(191, 160) << "," << din(223, 192) << "," << din(255, 224) << ")\n"
+                      << "B: (" << din(287, 256) << "," << din(319, 288) << "," << din(351, 320) << "," << din(384, 352) << ","
+                                << din(415, 385) << "," << din(447, 416) << "," << din(479, 448) << "," << din(511, 480) << ")\n" 
+                      << "output : (" << (dout(31, 0)) << ")\n"
+                      << "expected : (" << (expected_output(31, 0)) << ")\n";
             pass = 0;
             break;
         }
     }
+
     if (pass)
         std::cout << "Success: results match" << std::endl;
 
