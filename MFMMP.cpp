1850124d254091ef9c1bcf5e80f3d5d82e791560
balaji-venkatesh <b.venkatesh@mail.utoronto.ca>
2023-06-29 10:13:26 -0400

    done with testing

--- MFMMP.cpp
+++ MFMMP.cpp
@@ -1,22 +1,23 @@
 #include "ap_int.h"
 
-void MFMMP(ap_uint<512> &din, ap_uint<32> &dout) {
+#define MFMMP_SIZE 16
+#define NUM_SIZE 32
+
+void MFMMP(ap_uint<1024> &din, ap_uint<NUM_SIZE> &dout) {
 
 #pragma HLS pipeline
 #pragma HLS interface axis port = din
 #pragma HLS interface axis port = dout
 #pragma HLS interface ap_ctrl_none port = return // removes start and finish signals - starts when there is data
 
-    // multiplier
-    ap_uint<256> C;
-    for (int i = 0; i < 8; i++) {
+    ap_uint<32> dout_tmp = 0;
+
+    ap_uint<NUM_SIZE> C [MFMMP_SIZE];
+    for (int i = 0; i < MFMMP_SIZE; i++) {
 #pragma HLS unroll
-        C(i * 32 + 31, i * 32) = ap_uint<32>(din(i * 32 + 31, i * 32)) * ap_uint<32>(din(256 + i * 32 + 31, 256 + i * 32)); // C = A * B
+        C[i] = ap_uint<32>(din(i * 32 + 31, i * 32)) * ap_uint<32>(din(512 + i * 32 + 31, 512 + i * 32)); // C = A * B
+        dout_tmp = dout_tmp + C[i];
     }
-
-    // adder tree
-    ap_uint<32> dout_tmp;
-    dout_tmp = ((C(31, 0) + C(63, 32)) + (C(95, 64) + C(127, 96))) + ((C(159, 128) + C(191, 160)) + (C(223, 192) + C(255, 224)));
-
+    
     dout = dout_tmp;
 }
--- MFMMP_tb.cpp
+++ MFMMP_tb.cpp
@@ -2,49 +2,53 @@
 #include <cstdlib>
 #include <iostream>
 
-#define MFMMP_SIZE 8
+#define MFMMP_SIZE 16
+#define NUM_SIZE 32
 
-void MFMMP(ap_uint<512> &din, ap_uint<32> &dout);
+void MFMMP(ap_uint<1024> &din, ap_uint<32> &dout);
 
 int main() {
     
-    ap_uint<512> din;
+    ap_uint<1024> din;
     ap_uint<32> dout;
     ap_uint<32> expected_output;
 
     int pass = 1;
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 1; i++) {
 
-        for (int j = 0; j < 16; j++) 
+        for (int j = 0; j < MFMMP_SIZE * 2; j++) 
             din(j * 32 + 31, j * 32) = rand() % 100;
 
-        expected_output = din(31, 0)    * din(287, 256) 
-                        + din(63, 32)   * din(319, 288) 
-                        + din(95, 64)   * din(351, 320) 
-                        + din(127, 96)  * din(383, 352) 
-                        + din(159, 128) * din(415, 384) 
-                        + din(191, 160) * din(447, 416) 
-                        + din(223, 192) * din(479, 448) 
-                        + din(255, 224) * din(511, 480);
+        expected_output(31, 0)  = din(31, 0)    * din(31+512, 0+512)
+                                + din(63, 32)   * din(63+512, 32+512)
+                                + din(95, 64)   * din(95+512, 64+512)
+                                + din(127, 96)  * din(127+512, 96+512)
+                                + din(159, 128) * din(159+512, 128+512)
+                                + din(191, 160) * din(191+512, 160+512)
+                                + din(223, 192) * din(223+512, 192+512)
+                                + din(255, 224) * din(255+512, 224+512)
+                                + din(287, 256) * din(287+512, 256+512)
+                                + din(319, 288) * din(319+512, 288+512)
+                                + din(351, 320) * din(351+512, 320+512)
+                                + din(383, 352) * din(383+512, 352+512)
+                                + din(415, 384) * din(415+512, 384+512)
+                                + din(447, 416) * din(447+512, 416+512)
+                                + din(479, 448) * din(479+512, 448+512)
+                                + din(511, 480) * din(511+512, 480+512);
 
         MFMMP(din, dout);
 
-        if (dout != expected_output) {
-            std::cout << "Test failed at iteration " << i << "\n"
-                      << "A: (" << din(31, 0)    << "," << din(63, 32)   << "," << din(95, 64)   << "," << din(127, 96)  << ","
-                                << din(159, 128) << "," << din(191, 160) << "," << din(223, 192) << "," << din(255, 224) << ")\n"
-                      << "B: (" << din(287, 256) << "," << din(319, 288) << "," << din(351, 320) << "," << din(383, 352) << ","
-                                << din(415, 384) << "," << din(447, 416) << "," << din(479, 448) << "," << din(511, 480) << ")\n" 
-                      << "output : (" << (dout(31, 0)) << ")\n"
-                      << "expected : (" << (expected_output(31, 0)) << ")\n";
+        std::cout << "TEST " << i << "\n"
+                  << "  in : " << din << "\n"
+                  << "  out: " << dout << "\n"
+                  << "  exp: " << expected_output << "\n";
+        if (dout != expected_output)
             pass = 0;
-            break;
-        }
     }
 
     if (pass)
-        std::cout << "Success: results match" << std::endl;
+        std::cout << "ALL RESULTS MATCH" << std::endl;
 
     return 0;
 }
