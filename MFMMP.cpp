0948bf4cdaccd98182544b320c5310ede7f261a1
balaji-venkatesh <b.venkatesh@mail.utoronto.ca>
2023-07-13 11:15:32 -0400

    transfer

--- MFMMP.cpp
+++ MFMMP.cpp
@@ -1,23 +1,83 @@
 #include "ap_int.h"
+#include "hls_stream.h"
+#include "ap_axi_sdata.h"
 
-#define MFMMP_SIZE 16
-#define NUM_SIZE 32
+#define MA_SZ 16  // matrix size
+#define NO_SZ 32  // number size
+#define ST_SZ 512 // stream size
 
-void MFMMP(ap_uint<1024> &din, ap_uint<NUM_SIZE> &dout) {
+#define DWIDTH 32
+#define type ap_int<DWIDTH>
+typedef hls::axis<type, 0, 0, 0> pkt;
 
-#pragma HLS pipeline
-#pragma HLS interface axis port = din
-#pragma HLS interface axis port = dout
-#pragma HLS interface ap_ctrl_none port = return // removes start and finish signals - starts when there is data
+void MFMMP(hls::stream<pkt> &din, hls::stream<pkt> &dout){
 
-    ap_uint<32> dout_tmp = 0;
+    //#pragma HLS pipeline
+    #pragma HLS interface axis port = din
+    #pragma HLS interface axis port = dout
+    #pragma HLS interface ap_ctrl_none port = return // removes start and finish signals - starts when there is data
 
-    ap_uint<NUM_SIZE> C [MFMMP_SIZE];
-    for (int i = 0; i < MFMMP_SIZE; i++) {
-#pragma HLS unroll
-        C[i] = ap_uint<32>(din(i * 32 + 31, i * 32)) * ap_uint<32>(din(512 + i * 32 + 31, 512 + i * 32)); // C = A * B
+    pkt din_tmp;
+    pkt dout_tmp;
+
+    ap_uint<NO_SZ> A [MA_SZ];
+    ap_uint<NO_SZ> B [MA_SZ];
+    ap_uint<NO_SZ> C [MA_SZ];
+
+    // read in data for matrix A
+    for (int i = 0; i < MA_SZ; i++) {
+        #pragma HLS unroll
+        int j = i % (ST_SZ / NO_SZ / 2);
+        if(j == 0)
+            din_tmp = din.read();
+        A[i] = din_tmp(j * NO_SZ + NO_SZ - 1, j * NO_SZ);
+    }
+
+    // read in data for matrix B
+    for (int i = 0; i < MA_SZ; i++) {
+        #pragma HLS unroll
+        int j = i % (ST_SZ / NO_SZ / 2);
+        if(j == 0)
+            din.read(din_tmp);
+        B[i] = din_tmp(j * NO_SZ + NO_SZ - 1, j * NO_SZ);
+    }
+
+    // other dout_tmps
+    pkt dout_tmp_1;
+
+    // multiply matrices and sum full
+    dout_tmp = 0;
+    for (int i = 0; i < MA_SZ/2; i++) {
+        #pragma HLS unroll
+        C[i] = A[i] + B[i]; // C = A * B
+        dout_tmp = dout_tmp + C[i];
+    }
+    dout_tmp1 = 0;
+    for (int i = MA_SZ/2; i < MA_SZ; i++) {
+        #pragma HLS unroll
+        C[i] = A[i] + B[i]; // C = A * B
+        dout_tmp1 = dout_tmp_1 + C[i];
+    }
+    dout.write(dout_tmp);
+    dout.write(dout_tmp1);
+
+
+    // multiply matrices and sum to half
+    dout_tmp = 0;
+    for (int i = 0; i < MA_SZ; i++) {
+        #pragma HLS unroll
+        C[i] = A[i] + B[i]; // C = A * B
+        dout_tmp = dout_tmp + C[i];
+    }
+
+    // multiply matrices and sum quarter
+    dout_tmp = 0;
+    for (int i = 0; i < MA_SZ; i++) {
+        #pragma HLS unroll
+        C[i] = A[i] + B[i]; // C = A * B
         dout_tmp = dout_tmp + C[i];
     }
     
-    dout = dout_tmp;
+    dout.write(dout_tmp);
+
 }
--- MFMMP_tb.cpp
+++ MFMMP_tb.cpp
@@ -2,47 +2,75 @@
 #include <cstdlib>
 #include <iostream>
 
-#define MFMMP_SIZE 16
-#define NUM_SIZE 32
+#include "hls_stream.h"
+#include "ap_axi_sdata.h"
 
-void MFMMP(ap_uint<1024> &din, ap_uint<32> &dout);
+#define MA_SZ 16  // matrix size
+#define NO_SZ 32  // number size
+#define ST_SZ 512 // stream size
+
+typedef ap_axiu<512, 0, 0, 0> pkt;
+
+void MFMMP(hls::stream<pkt> &din, hls::stream<pkt> &dout);
+
+#pragma HLS interface axis port = din
+#pragma HLS interface axis port = dout
 
 int main() {
-    
-    ap_uint<1024> din;
-    ap_uint<32> dout;
-    ap_uint<32> expected_output;
+  
+    ap_uint<512> din;
+    ap_uint<512> dout;
+    ap_uint<32> expected_output = 0;
+
+    ap_uint<NO_SZ> A [MA_SZ];
+    ap_uint<NO_SZ> B [MA_SZ];
+    //ap_uint<NO_SZ> C [MA_SZ];
 
     int pass = 1;
 
     for (int i = 0; i < 1; i++) {
 
-        for (int j = 0; j < MFMMP_SIZE * 2; j++) 
-            din(j * 32 + 31, j * 32) = rand() % 100;
+        std::cout << "TEST " << i << "\n";
 
-        expected_output(31, 0)  = din(31,  0)   * din(31+512,  0+512)
-                                + din(63,  32)  * din(63+512,  32+512)
-                                + din(95,  64)  * din(95+512,  64+512)
-                                + din(127, 96)  * din(127+512, 96+512)
-                                + din(159, 128) * din(159+512, 128+512)
-                                + din(191, 160) * din(191+512, 160+512)
-                                + din(223, 192) * din(223+512, 192+512)
-                                + din(255, 224) * din(255+512, 224+512)
-                                + din(287, 256) * din(287+512, 256+512)
-                                + din(319, 288) * din(319+512, 288+512)
-                                + din(351, 320) * din(351+512, 320+512)
-                                + din(383, 352) * din(383+512, 352+512)
-                                + din(415, 384) * din(415+512, 384+512)
-                                + din(447, 416) * din(447+512, 416+512)
-                                + din(479, 448) * din(479+512, 448+512)
-                                + din(511, 480) * din(511+512, 480+512);
-
+        // randomize and calculate sum of products
+        for (int j = 0; j < MA_SZ; j++){
+            A[j] = rand() % 128;
+            B[j] = rand() % 128;
+            expected_output = expected_output + A[j] * B[j];
+        }
+        
         MFMMP(din, dout);
 
-        std::cout << "TEST " << i << "\n"
-                  << "  in : " << din << "\n"
-                  << "  out: " << dout << "\n"
-                  << "  exp: " << expected_output << "\n";
+        ap_uint<ST_SZ> din_tmp;
+
+        // read in data for matrix A
+        std::cout << "A: " << "\n";
+        for (int j = 0; j < MA_SZ; j++){
+            int k = j % (ST_SZ / NO_SZ / 2);
+            din_tmp(k * 32 + 31, k * 32) = A[j];
+            std::cout << "\t" << A[j];
+            if(k == 7){
+                din.write(din_tmp)
+                std::cout << "\n";
+            }
+        }
+        // read in data for matrix B
+        std::cout << "B: " << "\n";
+        for (int j = 0; j < MA_SZ; j++){
+            int k = j % (ST_SZ / NO_SZ / 2);
+            din_tmp(k * 32 + 31, k * 32) = B[j];
+            std::cout << "\t" << B[j];
+            if(k == 7){
+                din.write(din_tmp)
+                std::cout << "\n";
+            }
+        }
+
+        ap_uint<NO_SZ> dout_tmp;
+        dout.read(dout_tmp);
+        std::cout << "OUT: " << "\n\t" << dout_tmp << "\n";
+        std::cout << "EXP: " << "\n\t" << expected_output << "\n";
+
         if (dout != expected_output)
             pass = 0;
     }
